43d9b4493790a58a66a75a8cb526cec91486fb06
Saloni Agrawal <saloniagrawal@Salonis-MacBook-Pro.local>
2018-06-23 20:55:24 -0700

    Dank green button

--- music/src/index.js
+++ music/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+class Note extends React.Component {
+  constructor(){
+  	super();
+
+  	this.state = {
+  		enabled: false
+  	}
+  }
+  setEnabled() {
+  	this.setState({enabled: !this.state.enabled}); 
+  }
+  render() {
+  	let btn_class = this.state.enabled ? "note-enabled" : "note-disabled";
+    return (
+      <button className={btn_class} onClick={this.setEnabled.bind(this)}>
+      	js
+      </button>
+    );
+  }
+}
+
+
+
 
 ReactDOM.render(
-	<div> sdkfjkslf </div>,
+	<div>
+		<Note />
+	</div>,
 	document.getElementById('root'),
 );
 
